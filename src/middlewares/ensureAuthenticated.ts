a7b769cb77eb00613859751f6debaa39717768e4
sidneiarauj <sidnei.ifrn@gmail.com>
2022-05-14 04:39:20 -0300

    implemented authentication middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/repositories/UserRepository';
+
+interface IPayload {
+    sub: string;
+}
+
+export async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(token, 'cf1aaac4c997f0083c20268ae6d108fd') as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
+    }
+
+    request.user = {
+      id: user_id,
+    };
+    next();
+  } catch {
+    throw new AppError('Invalid token!', 401);
+  }
+}
